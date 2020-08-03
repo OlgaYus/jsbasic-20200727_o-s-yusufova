fd845f2597d351550c90cd83ca4c654ae5f49ca0
Olga Yusufova <o.s.yusufova@gmail.com>
2020-08-03 09:30:55 +0300

    Olga
    Olga

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -4,6 +4,14 @@
  * @param {number} n
  * @returns {number}
  */
+
 function sum(m, n) {
   return m + n;
 }
+/**alert(`1 + 1 = ${sum(1, 1)}.`);
+alert(`2 + 5 = ${sum(2, 5)}.`);**/
+
+let result = sum(1, 1);
+alert(result); 
+let result = sum(2, 5);
+alert(result); 
